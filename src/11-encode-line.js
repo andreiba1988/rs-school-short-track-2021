cfbafc316f137e0a4b6568aaaa5143a6c0a143ff
andreiba1988 <71286389+andreiba1988@users.noreply.github.com>
2021-04-12 09:42:59 +0300

    Update 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  let result = '';
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count += 1;
+    } else if (count > 1) {
+      result += count + arr[i];
+      count = 1;
+    } else {
+      result += arr[i];
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
